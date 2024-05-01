bf723c47c623666191b3553d80bb179b8b4c4272
Mohammad <zare.6984@gmail.com>
2024-05-01 21:21:10 +0330

    Initial commit

---
+++ src/main/java/com/example/web_hw1/WebHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.web_hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_hw1/WebHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
