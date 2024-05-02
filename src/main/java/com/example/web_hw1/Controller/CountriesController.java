ca25eedb3f89aa918f5a27248a12bde3662a5952
Mohammad <zare.6984@gmail.com>
2024-05-03 00:08:44 +0330

    adding jackson & mariaDB dependency moving new files to correct packages

---
+++ src/main/java/com/example/web_hw1/Controller/CountriesController.java
@@ -0,0 +1,15 @@
+package com.example.web_hw1.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/countries")
+@RestController
+public class CountriesController {
+
+
+
+
+}
