6a1fb6a41bc3fa4e4f26e02d747a58debd958918
Alpha-Lima-India <147832944+Alpha-Lima-India@users.noreply.github.com>
2024-05-02 09:48:32 +0330

    Add files via upload

---
+++ Web_HW1-master/src/main/java/com/example/web_hw1/controller/countriesController.java
@@ -0,0 +1,17 @@
+package com.example.web_hw1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/countries")
+@RestController
+public class countriesController {
+
+    @GetMapping()
+
+
+}
