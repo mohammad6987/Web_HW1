0415d10a130f40ed1bac4a2eddfb7c6b50f12a73
Mohammad <zare.6984@gmail.com>
2024-05-04 00:39:50 +0330

    updating SecurityConfig

--- src/main/java/com/example/web_hw1/Config/SecurityConfig.java
+++ src/main/java/com/example/web_hw1/Config/SecurityConfig.java
@@ -1,8 +1,31 @@
 package com.example.web_hw1.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.
+                csrf(csrf -> csrf.disable()).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER)).
+                authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("test").
+                        password("123").build()
+        );
+    }
     
 }
