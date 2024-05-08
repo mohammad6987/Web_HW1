1ef031d19deedbb7ecda38cfc59acd9fba9884ce
Ali Neamt Doost <alinematdoost82@gmail.com>
2024-05-08 23:06:52 +0430

    EndPoint 2 incompleted

--- src/main/java/com/example/web_hw1/Controller/CountriesController.java
+++ src/main/java/com/example/web_hw1/Controller/CountriesController.java
@@ -1,16 +1,12 @@
 package com.example.web_hw1.Controller;
 
 
-import com.example.web_hw1.Model.Country;
-import com.example.web_hw1.Model.CountryContainer;
-import com.example.web_hw1.Model.CountryDto;
-import com.example.web_hw1.Model.datatype;
+import com.example.web_hw1.Model.*;
 import com.example.web_hw1.Service.WeatherService;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -22,21 +18,13 @@ import java.util.List;
 @RestController
 public class CountriesController {
 
-//    private RestTemplate restTemplate;
-//    private final org.springframework.http.HttpHeaders headers = new HttpHeaders();
-//
-//    public CountriesController(RestTemplate restTemplate){
-//        this.restTemplate = restTemplate;
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-////    }
-//
-//    private final WebClient webClient;
-//
     private final RestTemplate restTemplate;
+    private final HttpHeaders headers = new HttpHeaders();
+    private final String API_KEY = "LIPQv0O9lPlFFgxNslVs5g==sQ6DOXmXmTdOZPCm";
+
     public CountriesController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-//        this.webClient = builder.baseUrl("https://countriesnow.space/api/v0.1").build();
-
+        headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
 
@@ -49,5 +37,19 @@ public class CountriesController {
             countries.add(new CountryDto(country.getCountry()));
         }
         return new CountryContainer(countries,countries.size());
+    }
+
+    @GetMapping("/countries/{name}")
+    public Country[] getCountryByName(@PathVariable String name) {
+        String url = "https://api.api-ninjas.com/v1/country?name=" + name;
+//        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-api-key", API_KEY);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<> response
+        //        headers.set("x-api-key", API_KEY);
+////        listOfProperty listOfTwoDataTypes = restTemplate.getForEntity(url, listOfProperty.class).getBody();
+////        return listOfTwoDataTypes;
+//        Country[] listOfCountry = restTemplate.getForEntity(url, Country[].class).getBody();
+//        return listOfCountry;
     }
 }
---
+++ src/main/java/com/example/web_hw1/Model/Currency.java
@@ -0,0 +1,9 @@
+package com.example.web_hw1.Model;
+
+import lombok.Data;
+
+@Data
+public class Currency {
+    private String code;
+    private String name;
+}
