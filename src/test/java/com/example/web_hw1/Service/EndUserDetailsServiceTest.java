f1a5b7bcca096c3df6ef8847aafd92d890875903
Mohammad <zare.6984@gmail.com>
2024-05-12 01:23:43 +0330

    adding junit and a simple unit test

---
+++ src/test/java/com/example/web_hw1/Service/EndUserDetailsServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.web_hw1.Service;
+
+import com.example.web_hw1.Model.EndUser;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.security.NoSuchAlgorithmException;
+
+public class EndUserDetailsServiceTest {
+
+    EndUserDetailsService endUserDetailsService;
+    @Test
+    void passwordHashTest() throws NoSuchAlgorithmException {
+        endUserDetailsService = new EndUserDetailsService(null , null , null);
+        String in = "qwerty";
+        Assertions.assertEquals("65E84BE33532FB784C48129675F9EFF3A682B27168C0EA744B2CF58EE02337C5" ,endUserDetailsService.hashString(in) );
+        in  = "123456789";
+        Assertions.assertEquals(endUserDetailsService.hashString(in) , "15E2B0D3C33891EBB0F1EF609EC419420C20E320CE94C65FBC8C3312448EB225");
+    }
+
+}
